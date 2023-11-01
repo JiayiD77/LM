b66d2be864abf17b31ec36606dbacf8b259d62da
JiayiD77 <114614626+JiayiD77@users.noreply.github.com>
2023-11-01 13:31:28 -0700

    Concurrent global test

--- concurrent_test.py
+++ concurrent_test.py
@@ -1,17 +1,35 @@
 import concurrent.futures
-from functools import partial
+from collections import defaultdict
 
-lst = []
+class NeuronDict:
+    def __init__(self):
+        self.dict = defaultdict(list)
+    
+    def add_item(self, workers):  
+        for i in range(workers):
+            post = 10  
+            for j in range(5):
+                self.dict[str(j)].append((str(post), 1))
+                post += 1
 
-def add(number):
-    return [n for n in range(number)]
+    def add_item_concurrent(self, workers):
+        post = 10
+
+        def add(post):
+            for j in range(5):
+                self.dict[str(j)].append((str(post), 1))
+                post += 1
+
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            results = executor.map(add, [post for i in range(workers)])
+            for result in results:
+                print(result)
 
 def main():
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        results = executor.map(add, range(3))
-        for result in results:
-            lst.extend(result)
-    print(lst)
+    nd = NeuronDict()
+    nd.add_item_concurrent(3)
+    print(nd.dict.items())
+
 
 if __name__ == "__main__":
     main()
